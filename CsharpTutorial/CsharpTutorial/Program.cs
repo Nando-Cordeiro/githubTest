fc91bbd16038e58f644e1d8e941434fc2e9e0220
Nando-Cordeiro <103462917+Nando-Cordeiro@users.noreply.github.com>
2022-04-29 17:26:51 -0500

    add vs project
    added vs project

---
+++ CsharpTutorial/CsharpTutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CsharpTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student()
+        
+
+            Console.ReadLine();
+        }
+    
+    }
+
+}
